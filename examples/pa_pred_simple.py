dcf00dafaf84e9ae714bcd3a0c685398159adb4c
Joshua Silver <joshua.silver@gmail.com>
2021-03-07 17:56:18 -0800

    Simplified example

--- examples/pa_pred_simple.py
+++ examples/pa_pred_simple.py
@@ -9,28 +9,19 @@ from singlearity import State, Player, Team, Venue, Atmosphere, Matchup, ApiExce
 from singlearity.rest import ApiException
 
 import pandas as pd
-pd.options.display.max_rows = 999
 
-###############################################################
-# First, validate that the API key is working
-###############################################################
-sing.hello_with_key()
-
-def show_example():
-    batter_list = ['Mookie Betts']
-    pitcher_list  = ['Chris Paddack']
-
-    candidate_batters = [sing.get_players(name = player_name)[0] for player_name in batter_list]
-    candidate_pitchers = [sing.get_players(name = player_name)[0] for player_name in pitcher_list]
-    venue = sing.get_venues(stadium_name = "Dodger Stadium")[0]
-    atmosph = Atmosphere(venue, temperature = 70, home_team = sing.get_teams(name = "Dodgers")[0])
-
-    matchups = ([Matchup(batter = m, pitcher = p, 
+batter_list = ['Mookie Betts', 'Cody Bellinger', 'Gavin Lux']
+pitcher_list  = ['Chris Paddack']
+candidate_batters = [sing.get_players(name = player_name)[0] for player_name in batter_list]
+candidate_pitchers = [sing.get_players(name = player_name)[0] for player_name in pitcher_list]
+venue = sing.get_venues(stadium_name = "Dodger Stadium")[0]
+atmosph = Atmosphere(venue, temperature = 70, home_team = sing.get_teams(name = "Dodgers")[0])
+matchups = ([Matchup(batter = m, pitcher = p, 
         atmosphere = atmosph, state=State()) for m in candidate_batters for p in candidate_pitchers])
-    results = pd.DataFrame(sing.get_pa_sim(matchups))
-    print(f'\nResults')
-    print(results.sort_values(by=["woba_exp"], ascending = False))
+
+results = pd.DataFrame(sing.get_pa_sim(matchups))
+#just get some interesting stats.  comment below line out to see all predictions
+results = results[['batter_name', 'pitcher_name', 'hr_exp', 'so_exp', 'ba_exp', 'ops_exp',  'woba_exp']]
+print(f'Results:\n{results}')
     
-if __name__ == '__main__':
-    show_example()
 
